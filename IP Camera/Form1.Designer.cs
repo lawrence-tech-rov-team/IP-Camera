1dcd573de6c91290b606c487f8fee614b30e3cfb
zcarey <zcarey@R8401983>
2019-08-04 13:14:54 -0400

    It *somehow* works.

--- IP Camera/Form1.Designer.cs
+++ IP Camera/Form1.Designer.cs
@@ -24,12 +24,131 @@
 		/// </summary>
 		private void InitializeComponent() {
 			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.ConnectBtn = new System.Windows.Forms.Button();
+			this.CameraTimer = new System.Windows.Forms.Timer(this.components);
+			this.UsernameTextBox = new System.Windows.Forms.TextBox();
+			this.PasswordTextBox = new System.Windows.Forms.TextBox();
+			this.ProfilesListBox = new System.Windows.Forms.ListBox();
+			this.IpAdressTextBox = new System.Windows.Forms.TextBox();
+			this.InfoLabel = new System.Windows.Forms.Label();
+			this.VideoPlayer = new Vlc.DotNet.Forms.VlcControl();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.VideoPlayer)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// ConnectBtn
+			// 
+			this.ConnectBtn.Location = new System.Drawing.Point(12, 12);
+			this.ConnectBtn.Name = "ConnectBtn";
+			this.ConnectBtn.Size = new System.Drawing.Size(86, 31);
+			this.ConnectBtn.TabIndex = 1;
+			this.ConnectBtn.Text = "Connect";
+			this.ConnectBtn.UseVisualStyleBackColor = true;
+			this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
+			// 
+			// CameraTimer
+			// 
+			this.CameraTimer.Interval = 1000;
+			// 
+			// UsernameTextBox
+			// 
+			this.UsernameTextBox.Location = new System.Drawing.Point(210, 21);
+			this.UsernameTextBox.Name = "UsernameTextBox";
+			this.UsernameTextBox.Size = new System.Drawing.Size(100, 22);
+			this.UsernameTextBox.TabIndex = 2;
+			// 
+			// PasswordTextBox
+			// 
+			this.PasswordTextBox.Location = new System.Drawing.Point(210, 49);
+			this.PasswordTextBox.Name = "PasswordTextBox";
+			this.PasswordTextBox.Size = new System.Drawing.Size(100, 22);
+			this.PasswordTextBox.TabIndex = 3;
+			// 
+			// ProfilesListBox
+			// 
+			this.ProfilesListBox.FormattingEnabled = true;
+			this.ProfilesListBox.ItemHeight = 16;
+			this.ProfilesListBox.Location = new System.Drawing.Point(12, 49);
+			this.ProfilesListBox.Name = "ProfilesListBox";
+			this.ProfilesListBox.Size = new System.Drawing.Size(142, 260);
+			this.ProfilesListBox.TabIndex = 4;
+			this.ProfilesListBox.SelectedIndexChanged += new System.EventHandler(this.ProfilesListBox_SelectedIndexChanged);
+			// 
+			// IpAdressTextBox
+			// 
+			this.IpAdressTextBox.Location = new System.Drawing.Point(104, 21);
+			this.IpAdressTextBox.Name = "IpAdressTextBox";
+			this.IpAdressTextBox.Size = new System.Drawing.Size(100, 22);
+			this.IpAdressTextBox.TabIndex = 5;
+			// 
+			// InfoLabel
+			// 
+			this.InfoLabel.AutoSize = true;
+			this.InfoLabel.Location = new System.Drawing.Point(12, 316);
+			this.InfoLabel.Name = "InfoLabel";
+			this.InfoLabel.Size = new System.Drawing.Size(31, 17);
+			this.InfoLabel.TabIndex = 6;
+			this.InfoLabel.Text = "info";
+			// 
+			// VideoPlayer
+			// 
+			this.VideoPlayer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.VideoPlayer.BackColor = System.Drawing.Color.Black;
+			this.VideoPlayer.Location = new System.Drawing.Point(316, 20);
+			this.VideoPlayer.Name = "VideoPlayer";
+			this.VideoPlayer.Size = new System.Drawing.Size(643, 418);
+			this.VideoPlayer.Spu = -1;
+			this.VideoPlayer.TabIndex = 7;
+			this.VideoPlayer.Text = "vlcControl1";
+			this.VideoPlayer.VlcLibDirectory = ((System.IO.DirectoryInfo)(resources.GetObject("VideoPlayer.VlcLibDirectory")));
+			this.VideoPlayer.VlcMediaplayerOptions = null;
+			this.VideoPlayer.VlcLibDirectoryNeeded += new System.EventHandler<Vlc.DotNet.Forms.VlcLibDirectoryNeededEventArgs>(this.VideoPlayer_VlcLibDirectoryNeeded);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(226, 405);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 8;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(971, 450);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.VideoPlayer);
+			this.Controls.Add(this.InfoLabel);
+			this.Controls.Add(this.IpAdressTextBox);
+			this.Controls.Add(this.ProfilesListBox);
+			this.Controls.Add(this.PasswordTextBox);
+			this.Controls.Add(this.UsernameTextBox);
+			this.Controls.Add(this.ConnectBtn);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.VideoPlayer)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+		private System.Windows.Forms.Button ConnectBtn;
+		private System.Windows.Forms.Timer CameraTimer;
+		private System.Windows.Forms.TextBox UsernameTextBox;
+		private System.Windows.Forms.TextBox PasswordTextBox;
+		private System.Windows.Forms.ListBox ProfilesListBox;
+		private System.Windows.Forms.TextBox IpAdressTextBox;
+		private System.Windows.Forms.Label InfoLabel;
+		private Vlc.DotNet.Forms.VlcControl VideoPlayer;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
--- IP Camera/Form1.cs
+++ IP Camera/Form1.cs
@@ -2,16 +2,120 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Vlc.DotNet.Core;
 
 namespace IP_Camera {
 	public partial class Form1 : Form {
+
+		UriBuilder deviceUri;
+		Media.Ver10.MediaClient media;
+		Media.Ver10.Profile[] profiles;
+
+		//TODO media 20;
+		//Media.Ver20.Media2Client media;
+		//Media.Ver20.MediaProfile[] profiles;
+
 		public Form1() {
 			InitializeComponent();
 		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			CameraTimer.Start();
+			//webBrowser1.Navigate("http://192.168.0.101/cgi-bin/hi3510/snap.cgi?&-getstream&-chn=1");
+			//Process.Start("chrome.exe", "http://www.YourUrl.com");
+		}
+
+		private void ConnectBtn_Click(object sender, EventArgs e) {
+			ProfilesListBox.Items.Clear();
+			InfoLabel.Text = "";
+
+			deviceUri = new UriBuilder("http:/onvif/device_service");
+			string[] addr = IpAdressTextBox.Text.Split(':');
+			deviceUri.Host = addr[0];
+			if (addr.Length == 2)
+				deviceUri.Port = Convert.ToInt16(addr[1]);
+
+			System.ServiceModel.Channels.Binding binding;
+			HttpTransportBindingElement httpTransport = new HttpTransportBindingElement();
+			httpTransport.AuthenticationScheme = System.Net.AuthenticationSchemes.Digest;
+			binding = new CustomBinding(new TextMessageEncodingBindingElement(MessageVersion.Soap12WSAddressing10, Encoding.UTF8), httpTransport);
+
+
+			Device.DeviceClient device = new Device.DeviceClient(binding, new EndpointAddress(deviceUri.ToString()));
+			Device.Service[] services = device.GetServices(false);
+
+			//TODO media 20
+			//Device.Service xmedia = services.FirstOrDefault(s => s.Namespace == "http://www.onvif.org/ver20/media/wsdl");
+			Device.Service xmedia = services.FirstOrDefault(s => s.Namespace == "http://www.onvif.org/ver10/media/wsdl");
+			if (xmedia != null) {
+				//TODO media 20
+				//media = new Media.Ver20.Media2Client(binding, new EndpointAddress(deviceUri.ToString()));
+				media = new Media.Ver10.MediaClient(binding, new EndpointAddress(deviceUri.ToString()));
+				media.ClientCredentials.HttpDigest.ClientCredential.UserName = UsernameTextBox.Text;
+				media.ClientCredentials.HttpDigest.ClientCredential.Password = PasswordTextBox.Text;
+				media.ClientCredentials.HttpDigest.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Impersonation;
+
+				//TODO media 20
+				//profiles = media.GetProfiles(null, null);
+				profiles = media.GetProfiles();
+				if (profiles != null)
+					foreach (var p in profiles)
+						ProfilesListBox.Items.Add(p.Name);
+			} else {
+				MessageBox.Show("No media was found.");
+			}
+
+			VideoPlayer.EndInit();
+		}
+
+		private void VideoPlayer_VlcLibDirectoryNeeded(object sender, Vlc.DotNet.Forms.VlcLibDirectoryNeededEventArgs e) {
+			if(IntPtr.Size == 4) {
+				e.VlcLibDirectory = new DirectoryInfo(@"C:\Program Files\VideoLAN\VLC"); //The folder where VLC is installed
+			}
+		}
+
+		private void ProfilesListBox_SelectedIndexChanged(object sender, EventArgs e) {
+			if(profiles != null && ProfilesListBox.SelectedIndex > 0) {
+				//TODO media 20
+				//UriBuilder uri = new UriBuilder(media.GetStreamUri("RtspOverHttp", profiles[ProfilesListBox.SelectedIndex].token));
+				//Media.Ver10.Transport tunnel = new Media.Ver10.Transport();
+				//tunnel.Protocol = Media.Ver10.TransportProtocol.HTTP;
+
+				Media.Ver10.Transport transport = new Media.Ver10.Transport();
+				transport.Protocol = Media.Ver10.TransportProtocol.RTSP;
+				//transport.Tunnel = tunnel;
+
+				Media.Ver10.StreamSetup streamSetup = new Media.Ver10.StreamSetup();
+				streamSetup.Transport = transport;
+
+				UriBuilder uri = new UriBuilder(media.GetStreamUri(streamSetup, profiles[ProfilesListBox.SelectedIndex].token).Uri);
+				uri.Host = deviceUri.Host;
+				uri.Port = deviceUri.Port;
+				uri.Scheme = "rtsp";
+				InfoLabel.Text = uri.Path;
+
+				string[] options = { ":rtsp-http", ":rtsp-http-port=" + uri.Port, ":rtsp-user=" + UsernameTextBox.Text, ":rtsp-pwd=" + PasswordTextBox.Text, };
+
+				VideoPlayer.VlcMediaPlayer.Play(uri.Uri, options);
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			//string[] options = { ":rtsp-http", ":rtsp-http-port=" + uri.Port, ":rtsp-user=" + UsernameTextBox.Text, ":rtsp-pwd=" + PasswordTextBox.Text, };
+			//VideoPlayer.VlcMediaPlayer.Play(new Uri("rtsp://192.168.0.100/11"), options);
+			VideoPlayer.VlcMediaPlayer.Play(new Uri("rtsp://192.168.0.100/11"));
+			//VideoPlayer.VlcMediaPlayer.Play(new Uri("http://192.168.0.100/cgi-bin/hi3510/snap.cgi?&-getstream&-chn=1"));
+		}
 	}
+
 }
